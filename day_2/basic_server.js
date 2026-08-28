c7629f5c5045a6e6265dd5211b62cb8158a57922
tanvir <tanvirulislam149@gmail.com>
2026-08-28 18:50:40 +0600

    basic server

---
+++ day_2/basic_server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.end("hello world from node basic server")
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
