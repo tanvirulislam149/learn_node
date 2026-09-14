c8924a3f17eace846f9ce0549ce30b6cf6bbc055
tanvir <tanvirulislam149@gmail.com>
2026-09-14 22:39:15 +0600

    db connected

--- Mongoose/src/server.js
+++ Mongoose/src/server.js
@@ -1,9 +1,13 @@
 import express from "express"
+import connectDB from "./config/db.js";
 
 const app = express()
 const PORT = 3000;
 
 app.use(express.json())
+
+// Connect to DB
+connectDB()
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello world from mongoose");
