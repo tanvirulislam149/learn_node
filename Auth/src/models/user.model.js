2828c71db42857d5017ffce29918ab1b10cb1027
tanvir <tanvirulislam149@gmail.com>
2026-09-15 16:36:06 +0600

    user model done

---
+++ Auth/src/models/user.model.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+
+const UserModel = model("User", userSchema)
+
+export default UserModel
