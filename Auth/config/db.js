bcf8055d8be2b210227d8ec503319b15720848b7
tanvir <tanvirulislam149@gmail.com>
2026-09-15 16:32:29 +0600

    session setup

--- Auth/config/db.js
+++ Auth/config/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose"
+import "dotenv/config"
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/")
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log("Database connected");
     } catch (error) {
         console.log(error.message);
--- Auth/src/server.js
+++ Auth/src/server.js
@@ -1,10 +1,20 @@
 import express from "express"
 import connectDB from "../config/db.js"
+import 'dotenv/config'
+import session from "express-session"
 
 const app = express()
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 10  // 10 min
+    }
+}))
 
 // connect DB
 connectDB()
